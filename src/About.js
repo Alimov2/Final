2d3c9ebbcf18bd609870a112586bf268681d56d6
Alimov2 <119588443+Alimov2@users.noreply.github.com>
2023-06-24 21:29:14 +0600

    first commit

---
+++ src/About.js
@@ -0,0 +1,17 @@
+
+import HeroSection from './components/HeroSection'
+
+
+const About = () => {
+  const data={
+    name: "Suleimanov Hojiakbar"
+  }
+  return (
+    <>
+      <HeroSection myData={data} />
+    </>
+  
+  )
+}
+
+export default About
